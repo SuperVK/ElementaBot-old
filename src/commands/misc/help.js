1c378b2ccc64a51621b5cb9fae95157b04e88918
SuperVK <victor.klomp@gmail.com>
2019-02-09 21:59:21 +0100

    added help and commands

---
+++ src/commands/misc/help.js
@@ -0,0 +1,8 @@
+module.exports = {
+    desc: 'help plez',
+    aliases: ['help'],
+    run: function(message, client) {
+        message.channel.createMessage(`Do .commands for the commands
+        `)
+    }
+}
