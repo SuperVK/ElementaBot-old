14c0c5233b44dd083c85971e76455bcb8195dc98
SuperVK <victor.klomp@gmail.com>
2019-02-13 21:04:56 +0100

    made it so you can use the bot outside of ew server

--- src/events.js
+++ src/events.js
@@ -8,7 +8,7 @@ module.exports = {
 
         let [command, ...args] = message.content.toLowerCase().substring(client.prefix.length).trim().replace(/\s{2,}/g,' ').split(' ')
         message.args = args
-        if(!message.member.roles.includes(client.playerRoleID) && command != 'role') return message.channel.createMessage('You need a players role!')
+        if(!message.member.roles.includes(client.playerRoleID) && command != 'role' && message.channel.guild.id == '543144213588738059') return message.channel.createMessage('You need a players role!')
         let commandFunc = client.commands.find(cmd => cmd.aliases.includes(command))
         if(commandFunc == undefined) return
         if(client.discmds.includes(commandFunc.aliases[0]) && message.author.id !== client.ownerID) return message.channel.createMessage('This command is disabled right now')
